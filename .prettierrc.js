4e7aad4a7f970472cbd691f4aaae57dcaa843468
FD <2899385645@qq.com>
2023-04-24 19:23:32 +0800

    up

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,6 @@
 // https://github.com/FourteenD/config/blob/main/.prettierrc.default.js
 module.exports = {
-  printWidth: 100,
+  printWidth: 120,
   trailingComma: "all",
   htmlWhitespaceSensitivity: "strict",
   plugins: ["prettier-plugin-packagejson"],
