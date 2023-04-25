420f0d2a6ead3294d3d801ec801197bb84868db0
FourteenD <2899385645@qq.com>
2023-04-25 11:55:28 +0800

    up

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import Vue from "vue";
 import App from "./App.vue";
-import "./uni.promisify.adaptor";
+import "@/utils/uni.promisify.adaptor";
 
 Vue.config.productionTip = false;
 
-const app = new (typeof App === "function" ? App : Vue.extend(Object.assign({ mpType: "app" }, App)))();
+const app = new (
+  typeof App === "function"
+    ? App
+    : Vue.extend(Object.assign({ mpType: "app" }, App))
+)();
 
 app.$mount();
